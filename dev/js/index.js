45f326579796ed7a536afe5578b5d3f305090a4b
Shraddha <shraddha.patil@ost.com>
2019-10-04 19:27:43 +0530

    initial dump

---
+++ dev/js/index.js
@@ -0,0 +1,14 @@
+(function ( window ) {
+
+    var headElem = document.getElementsByTagName("head"),
+      prototypeVersion = parseInt(new URL(document.location).searchParams.get('v') || 1);
+    headElem[0].innerHTML += '<link type="text/css" rel="stylesheet" href="../../css/index.css?v=' + prototypeVersion + '"/>';
+
+    document.addEventListener("DOMContentLoaded",function () {
+      var headElem = document.getElementsByTagName("head");
+      // document.querySelector('body.branding-invision').style.background='#ffffff !important'
+      headElem[0].innerHTML += '<style>body.mobile {background: #ffffff !important;}</style>'
+    })
+
+
+})( window );
