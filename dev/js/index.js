04d3aad4df5e665343a9f192912b3285871569af
Shraddha <shraddha.patil@ost.com>
2019-10-04 20:18:55 +0530

    prototypes added

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,7 +2,7 @@
 
     var headElem = document.getElementsByTagName("head"),
       prototypeVersion = parseInt(new URL(document.location).searchParams.get('v') || 1);
-    headElem[0].innerHTML += '<link type="text/css" rel="stylesheet" href="../../css/index.css?v=' + prototypeVersion + '"/>';
+    headElem[0].innerHTML += '<link type="text/css" rel="stylesheet" href="../css/index.css?v=' + prototypeVersion + '"/>';
 
     document.addEventListener("DOMContentLoaded",function () {
       var headElem = document.getElementsByTagName("head");
